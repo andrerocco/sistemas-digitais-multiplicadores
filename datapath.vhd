4bfef0aeb5bb3c3b1a529654be28233fc43c4902
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-15 20:32:10 -0300

    Definidas conexões dos componentes do Datapath

--- datapath.vhd
+++ datapath.vhd
@@ -5,10 +5,10 @@ entity datapath is
     -- generic (N: integer := 8);
     port (
         CLK: in std_logic;
-        A, B: in std_logic_vector(7 downto 0); -- Números que vão ser multiplicados (Entradas de dados)
+        NUMA, NUMB: in std_logic_vector(3 downto 0); -- Números que vão ser multiplicados (Entradas de dados)
         SIG1, SIG2, SIG3: in std_logic; -- Sinais de controle (mP, cP, mA, cA, cB, cmult) onde
                                         -- (mP = mA = cB) é SIG1; (cP = cA) é SIG2; (cmult) é SIG3
-        RESULTADO: out std_logic_vector(7 downto 0); -- Saída de dados
+        RESULTADO: out std_logic_vector(3 downto 0); -- Saída de dados
         Az, Bz: out std_logic -- Sinais de status
     );
 end datapath;
@@ -49,20 +49,41 @@ architecture rtl of datapath is
 
 	signal saimux1, saimux2, saimux3, sairegP, sairegA, sairegB, saisomasub: std_logic_vector(3 downto 0); --(N-1 downto 0)
 
+    signal sai_somador, sai_mux1, sai_regP, sai_regB: std_logic_vector(3 downto 0);
+    signal sai_subtrator, sai_mux2, sai_regA: std_logic_vector(3 downto 0);
+
 begin
 
-    --MUX1: mux2para1 port map ( );
-	--REG_P: registrador_r port map ( );
-	--REG_A: registrador port map ( );
-	--REG_B: registrador port map ( );
-	--MUX2: mux2para1 port map ( );	
-	--MUX3: mux2para1 port map ( );
-	--SOMA_SUB: somadorsubtrator port map ( );
-	--GERA_Az: igualazero port map ( );
-	--GERA_Bz: igualazero port map ( );	
+    -- MULTIPLICAÇÂO (ESQUERDA DO CIRCUITO)
+
+    MUX1: mux2para1 port map (a => sai_somador, b => "0000",
+                              sel => SIG1, y => sai_mux1 );
+    
+    REG_P: registrador port map (clk => clk, carga => SIG2,
+                                 d => sai_mux1, q => sai_regP );
+    
+    REG_MULT: registrador port map (clk => clk, carga => SIG3,
+                                    d => sai_regP, q => RESULTADO );
+
+    REG_B: registrador port map (clk => clk, carga => SIG1,
+                                 d => NUMB, q => sai_regB );
+    
+    GERA_Bz: igualazero port map (a => sai_regB, igual => Bz );
+    
+    SOMA: somadorsubtrator port map (a => sai_regP, b => sai_regB,
+                                     op => '0', s => sai_somador);
+    
+    -- CONTAGEM DE MULTIPLICAÇÕES (DIREITA DO CIRCUITO)
+
+    MUX2: mux2para1 port map (a => sai_subtrator, b => NUMA,
+                              sel => SIG1, y => sai_mux2 );
 	
-	--saida <= sairegP;
-	--conteudoA <= sairegA;
-	--conteudoB <= sairegB;
+	REG_A: registrador port map (clk => clk, carga => SIG2,
+                                 d => sai_mux2, q => sai_regA );
+	
+	GERA_Az: igualazero port map (a => sai_regA, igual => Az );
+	
+	SUB: somadorsubtrator port map (a => sai_regA, b => "0001",
+                                    op => '1', s => sai_subtrator);
 
 end rtl;
