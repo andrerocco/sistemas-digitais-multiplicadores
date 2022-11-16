e3a9be5891217cf5c5bce55e6b3c35eae58f903d
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-16 20:58:01 -0300

    Criado somador com overflow e subtrator

---
+++ MultiplicadorArray/subtrator.vhd
@@ -0,0 +1,13 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY somadorsubtrator IS
+PORT (a, b : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+      s : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+END somadorsubtrator;
+
+ARCHITECTURE estrutura OF somadorsubtrator IS
+BEGIN
+    s <= a - b;
+END estrutura;
