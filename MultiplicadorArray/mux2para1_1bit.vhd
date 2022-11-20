48502d80edb33444ffb2730f86b3113455ff059e
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-20 01:11:55 -0300

    Removido comentário

--- MultiplicadorArray/mux2para1_1bit.vhd
+++ MultiplicadorArray/mux2para1_1bit.vhd
@@ -2,7 +2,6 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY mux2para1_1bit IS
-  -- GENERIC ( N : INTEGER );
   PORT ( a, b : IN STD_LOGIC;
          sel: IN STD_LOGIC;
          y : OUT STD_LOGIC);
