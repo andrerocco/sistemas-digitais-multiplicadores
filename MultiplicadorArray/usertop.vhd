9c5176dc3d3b7add1393f519a0e7a8a47a6a4627
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-20 00:43:47 -0300

    Conexão dos sinais do usertop

--- MultiplicadorArray/usertop.vhd
+++ MultiplicadorArray/usertop.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity usertop is
-    -- generic (N: integer := 8);
+    -- generic (N: integer := 4);
     port (
         CLK: in std_logic;
         A, B: in std_logic_vector(3 downto 0); -- Números que vão ser multiplicados
@@ -18,33 +18,47 @@ architecture rtl of usertop is
         port(
             CLK: in std_logic;
             RESET, INICIAR: in std_logic; -- Entradas de controle
-            PRONTO: out std_logic; -- Saída de controle
             Azero, Bzero, CONTz: in std_logic; -- Sinais de status (recebidos do Datapath)
-            UltimoBitA: in std_logic -- Sinal de status (recebido do Datapath)
+            UltimoBitA: in std_logic; -- Sinal de status (recebido do Datapath)
+            PRONTO: out std_logic; -- Saída de controle
+            SIG1, SIG2, ccont, cmult, cPH: out std_logic -- Sinais de controle (enviados para o Datapath)
         );
     end component;
 
     component datapath is
         port(
-            
+            CLK: in std_logic;
+            NUMA, NUMB: in std_logic_vector(3 downto 0); -- Números que vão ser multiplicados (Entradas de dados)
+            RESULTADO: out std_logic_vector(3 downto 0); -- Saída de dados
+            Az, Bz, CONTz: out std_logic; -- Sinais de status
+            UltimoBitA: out std_logic; -- Sinal de status
+            SIG1, SIG2, ccont, cmult, cPH: out std_logic -- Sinais de controle (enviados do Bloco de Controle)
+                                                     -- (mPH = cPL = cB = cA = mCONT = mFF) é SIG1
+                                                     -- (srPH = srPL = srA0) é SIG2
         );
     end component;
 
-    signal controle1, controle2, controle3: std_logic; -- Sinais de controle
-    signal status_Az, status_Bz: std_logic; -- Sinais de status
+    signal controle_SIG1, controle_SIG2, controle_ccont, controle_cmult, controle_cPH: std_logic; -- Sinais de controle
+    signal status_Az, status_Bz, status_CONTz, status_UltimoBitA: std_logic; -- Sinais de status
 
 begin
     
-    BLOCOCONTROL: control port map (/* CLK => CLK,
-                                    RESET => RESET, INICIAR => INICIAR,
-                                    Azero => status_Az, Bzero => status_Bz,
-                                    SIG1 => controle1, SIG2 => controle2, SIG3 => controle3,
-                                    PRONTO => PRONTO */ );
+    BLOCOCONTROL: control port map (
+        CLK => CLK,
+        RESET => RESET, INICIAR => INICIAR,
+        Azero => status_Az, Bzero => status_Bz, CONTz => status_CONTz,
+        UltimoBitA => status_UltimoBitA,
+        PRONTO => PRONTO,
+        SIG1 => controle_SIG1, SIG2 => controle_SIG2, ccont => controle_ccont, cmult => controle_cmult, cPH => controle_cPH
+    );
 
-    BLOCODATAPATH: datapath port map (/* CLK => CLK,
-                                      NUMA => A, NUMB => B,
-                                      SIG1 => controle1, SIG2 => controle2, SIG3 => controle3,
-                                      RESULTADO => RESULTADO,
-                                      Az => status_Az, Bz => status_Bz */ );
+    BLOCODATAPATH: datapath port map (
+        CLK => CLK,
+        NUMA => A, NUMB => B,
+        RESULTADO => RESULTADO,
+        Az => status_Az, Bz => status_Bz, CONTz => status_CONTz,
+        UltimoBitA => status_UltimoBitA,
+        SIG1 => controle_SIG1, SIG2 => controle_SIG2, ccont => controle_ccont, cmult => controle_cmult, cPH => controle_cPH
+    );
 
 end rtl;
