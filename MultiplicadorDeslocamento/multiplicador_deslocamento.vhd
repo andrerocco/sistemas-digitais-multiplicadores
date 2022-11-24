e17c5ee9387e956da01d9e0b9a6ac317efb78cef
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-23 21:12:05 -0300

    Atualização de imports

--- MultiplicadorDeslocamento/multiplicador_deslocamento.vhd
+++ MultiplicadorDeslocamento/multiplicador_deslocamento.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+use ieee.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all; 
 
 entity multiplicador_deslocamento is
